887f1a835d9ebd8e96ee9dc3cb89f590a5ec0724
juansalgado12 <juan.salgado02@uceva.edu.co>
2024-08-29 20:01:57 -0500

    cambios

--- curso00-temphumedad/src/main.cpp
+++ curso00-temphumedad/src/main.cpp
@@ -1,18 +1,121 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "DHT.h"
+#define DHTTYPE DHT11  // DHT 11
+
+const char* ssid = "Juanjo.mc";
+const char* password = "123456789";
+
+#define LED 2 // LED
+// DHT Sensor
+uint8_t DHTPin = 4;
+
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+
+float Temperature;
+float Humidity;
+
+WiFiServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ dht.begin();
+ Serial.begin(115200);
+ delay(10);
+
+ pinMode(LED, OUTPUT);
+ digitalWrite(LED, LOW); //LED apagado
+
+ // Connect to WiFi network
+ Serial.println();
+ Serial.println();
+ Serial.print("Connecting to ");
+ Serial.println(ssid);
+
+ WiFi.begin(ssid, password);
+
+ while (WiFi.status() != WL_CONNECTED) {
+   delay(500);
+   Serial.print(".");
+ }
+ Serial.println("");
+ Serial.println("WiFi connected");
+
+ // Start the server
+ server.begin();
+ Serial.println("Server started");
+
+ // Print the IP address
+ Serial.print("Use this URL to connect: ");
+ Serial.print("http://");
+ Serial.print(WiFi.localIP());
+ Serial.println("/");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+ Temperature = dht.readTemperature(); // Gets the values of the temperature
+ Humidity = dht.readHumidity(); // Gets the values of the humidity
+ // Check if a client has connected
+ WiFiClient client = server.available();
+ if (!client) {
+   return;
+ }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ // Wait until the client sends some data
+ Serial.println("new client");
+ while(!client.available()){
+   delay(1);
+ }
+
+ // Read the first line of the request
+ String request = client.readStringUntil('\r');
+ Serial.println(request);
+ client.flush();
+
+ // Match the request
+
+ int value = HIGH;
+ if (request.indexOf("/LED=ON") != -1)  {
+   digitalWrite(LED, HIGH);
+   value = HIGH;
+ }
+ if (request.indexOf("/LED=OFF") != -1)  {
+   digitalWrite(LED, LOW);
+   value = LOW;
+ }
+
+ // Return the response
+ client.println("HTTP/1.1 200 OK");
+ client.println("Content-Type: text/html");
+ client.println(""); //  do not forget this one
+ client.println("<!DOCTYPE HTML>");
+ client.println("<html>");
+
+ client.print("LED is now: ");
+
+ if(value == LOW) {
+   client.print("Off");
+ } else {
+   client.print("On");
+ }
+ client.println("<br><br>");
+ client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></arduino-sht>");
+ client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");
+ client.println("</html>");
+
+ client.println("<br><br>");
+ client.print("Temperature: ");
+ client.print(Temperature);
+ client.print("C");
+ client.println("<br><br>");
+ client.print("Humidity: ");
+ client.print(Humidity);
+ client.print("%");
+
+ delay(1);
+ Serial.println("Client disconnected");
+ Serial.println("");
+
 }
